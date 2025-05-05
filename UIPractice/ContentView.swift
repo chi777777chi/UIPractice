4ed9a84185fedfea7400b139afd85cb471cd5c73
chi777777chi <147385591+chi777777chi@users.noreply.github.com>
2025-05-05 15:28:03 +0800

    Initial Commit

---
+++ UIPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UIPractice
+//
+//  Created by 陳泓齊 on 2025/5/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UIPractice/UIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIPracticeApp.swift
+//  UIPractice
+//
+//  Created by 陳泓齊 on 2025/5/5.
+//
+
+import SwiftUI
+
+@main
+struct UIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
